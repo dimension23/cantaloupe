6e7562011a277106948a364864c6e1d8d2b7e4a5
Prashant <prashant1139@gmail.com>
2016-09-10 09:00:57 -0700

    u test

--- tests/hmmer-spark.sh
+++ tests/hmmer-spark.sh
@@ -1,9 +1,23 @@
 #!/bin/bash
-rm -rvf ../output/*.txt
+echo cleaning up output from previous job
+rm -rf ../output/*.txt
+rm -rf /tmp/seq
+echo spark job in progress
 spark-submit --master local[*] hmmer-spark.py file:///home/cloudera/cantaloupe/input/small.fasta
-rm -rvf ../output/*.txt
+echo spark job completed
+
+echo cleaning up output from previous job
+rm -rf ../output/*.txt
+rm -rf /tmp/seq
+echo spark job in progress
 spark-submit --master local[*] hmmer-spark.py file:///home/cloudera/cantaloupe/input/medium.fasta
-rm -rvf ../output/*.txt
+echo spark job completed
+
+echo cleaning up output from previous job
+rm -rf ../output/*.txt
+rm -rf /tmp/seq
+echo spark job in progress
 spark-submit --master local[*] hmmer-spark.py file:///home/cloudera/cantaloupe/input/large.fasta
+echo spark job completed
 
 
